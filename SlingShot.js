fa40edbfdcba3ad9af0b6d645e8cef983438b0e8
MishtiGogia <74185122+MishtiGogia@users.noreply.github.com>
2021-01-19 16:40:11 +0530

    Add files via upload

---
+++ SlingShot.js
@@ -0,0 +1,26 @@
+class SlingShot{
+    constructor(bodyA,pointB){
+       var options={
+        bodyA:bodyA,
+        pointB:pointB,
+        stiffness:0.1,
+        lenght:6
+       }
+       this.pointB= pointB;
+       this.sling= Constraint.create(options);
+       World.add(world,this.sling);
+
+    }
+    display(){
+    if(this.sling.bodyA){
+   
+    push();
+    strokeWeight(3);
+    line(this.sling.bodyA.position.x,this.sling.bodyA.position.y,this.pointB.x,this.pointB.y); 
+    pop();
+}
+    }
+    fly(){
+     this.sling.bodyA = null;   
+    }
+}
